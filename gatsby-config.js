6d21989943b1f8252e491950171d67316ff12d9a
giaduc <lgduc1913@gmail.com>
2020-03-10 18:10:42 +0700

    update site info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	siteMetadata: {
-		title: "Gatsby + Netlify CMS Starter",
+		title: "deptoong.shop - giày dép nam",
 		description:
-			"This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
+			"giày da nam, dép nam một quai, dép nam quai ngang, giày da công sở."
 	},
 	plugins: [
 		"gatsby-plugin-react-helmet",
@@ -10,7 +10,8 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-google-analytics`,
 			options: {
-				trackingId: "UA-144765607-2"
+				trackingId: "UA-144765607-2",
+				head: true
 			}
 		},
 		{
